cdf2c45fd6ec8a7efa03862a2cc95885afd5a49e
Ademide <alaseademide02@gmail.com>
2025-10-24 14:04:41 +0100

    Add Contact and About pages

--- contact.js
+++ contact.js
@@ -1,4 +1,6 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const name = document.getElementById("name");
@@ -7,19 +9,21 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
   const message = document.getElementById("message");
   const success = document.getElementById("success");
 
-  let valid = true;
-
   // Reset error messages
-  document.querySelectorAll(".error").forEach(el => el.textContent = "");
+  const errors = {
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  };
 
   // Full name validation
   if (name.value.trim() === "") {
-    document.getElementById("error-name").textContent = "Full name is required.";
-    valid = false;
+    errors.name = "Full name is required.";
   }
 
   // Email validation
-   if (email.value.trim() === "") {
+  if (email.value.trim() === "") {
     errors.email = "Email is required.";
   } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
     errors.email = "Please enter a valid email (e.g., name@example.com).";
@@ -27,18 +31,26 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
 
   // Subject validation
   if (subject.value.trim() === "") {
-    document.getElementById("error-subject").textContent = "Subject is required.";
-    valid = false;
+    errors.subject = "Subject is required.";
   }
 
   // Message validation
   if (message.value.trim().length < 10) {
-    document.getElementById("error-message").textContent = "Message must be at least 10 characters.";
-    valid = false;
+    errors.message = "Message must be at least 10 characters.";
   }
 
-  if (valid) {
-    success.textContent = "✅ Thank you! Your message has been sent successfully.";
+  // Display error messages
+  Object.keys(errors).forEach((el) => {
+    const error = document.getElementById(error-${el});
+    error.textContent = errors[el];
+  });
+
+  // display success message if valid
+  const hasError = Object.values(errors).some((msg) => msg !== "");
+
+  if (!hasError) {
+    success.textContent =
+      "✅ Thank you! Your message has been sent successfully.";
     document.getElementById("contactForm").reset();
   } else {
     success.textContent = "";
